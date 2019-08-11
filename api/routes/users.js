4c90eef772587d50a0a32e9a2904673a4e8398ad
Jeremy (MacBook Pro) <jeremy.e.beutler@gmail.com>
2019-08-11 15:39:03 -0600

    rewrote mongoose queries as mongodb driver queries in users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const mongodb = require('mongodb')
+const mongodb_connect = require('../../mongodb-connect');
 
-let mongodb_connect = require('../../mongodb-connect');
-console.log(mongodb_connect.db)
+const User = mongodb_connect.db.collection('users');
+const Event = mongodb_connect.db.collection('events')
+const OptIn = mongodb_connect.db.collection('optins')
+const Click = mongodb_connect.db.collection('clicks')
 
 router.get('/', async (req, res, next) => {
     try {
-        const users = await User.find()
+        const users = await User.find({}).toArray();
         res.status(200).json(users);
     } catch (error) {
         res.status(500).json({
@@ -17,81 +21,62 @@ router.get('/', async (req, res, next) => {
 
 router.post('/:userId/reviewEvent/:eventId', async (req, res, next) => {
     try {
-        let clicking_user = req.params.userId
-        let reviewed_event = req.params.eventId
-        let new_opt_ins = []
-        let new_clicks = []
-        let new_click_ids = []
-        let new_click__ids = []
-        // console.log(req.body.clicked_users)
+        let clicked_users = req.body.clicked_users
+        let clicked_users_oids = clicked_users.map(x => new mongodb.ObjectID(x))
+        let clicking_user_oid = new mongodb.ObjectID(req.params.userId)
+        let reviewed_event_oid = new mongodb.ObjectID(req.params.eventId)
+        var new_click_oids = []
 
         // Create opt-ins for each selected user
-        req.body.clicked_users.forEach(async (clicked_user) => {
-            const opt_in = new OptIn({
-                _id: new mongoose.Types.ObjectId(),
-                clicking_user: clicking_user,
-                clicked_user: clicked_user,
-                event: reviewed_event
-            })
-            await opt_in.save()
-            new_opt_ins.push(opt_in)
-        })
+        for (const clicked_user of clicked_users) {
+            const clicked_user_oid = new mongodb.ObjectID(clicked_user)
+            let opt_in = await OptIn.insertOne({
+                clicking_user: clicking_user_oid,
+                clicked_user: clicked_user_oid,
+                event: reviewed_event_oid
+            });
+        }
 
         // Find mutually opting users
-        let matched_opt_ins = await OptIn
-            .find({
-                event: reviewed_event, 
-                clicked_user: clicking_user,
-                clicking_user: { $in: req.body.clicked_users }
-            })
-            .select('clicking_user');
-        
-        console.log('matched_opt_ins')
-        console.log(matched_opt_ins)
-        
-        // Create click with each mutually opting user
-        // Update clicks array for each mutually opting user
-        matched_opt_ins.forEach(async (matched_opt_in) => {
-            let matched_clicking_user = matched_opt_in.clicking_user
-            let click = new Click({
-                _id: new mongoose.Types.ObjectId(),
-                user_1: clicking_user,
-                user_2: matched_clicking_user,
-                shared_events: [reviewed_event]
-            })
-            await click.save()
-            click_id = click.id
-
-            await User.findByIdAndUpdate(matched_clicking_user, { 
-                $push: { clicks: click_id }
-            })
-
-            new_clicks.push(click)
-            new_click_ids.push(click_id)
+        let matched_opt_ins = await OptIn.find({
+            event: reviewed_event_oid, 
+            clicked_user: clicking_user_oid,
+            clicking_user: { $in: clicked_users_oids }
         })
+        .project({ clicking_user: 1 })
+        .toArray();
 
-        console.log(new_click_ids)
+        // Create click with clicking user and each mutually opting user
+        // Update clicks array for each mutually opting user
+        for (const matched_opt_in of matched_opt_ins) {
+            const matched_clicking_user_oid = new mongodb.ObjectID(matched_opt_in.clicking_user)
+            let click = await Click.insertOne({
+                user_1: clicking_user_oid,
+                user_2: matched_clicking_user_oid,
+                shared_events: [reviewed_event_oid]
+            })
+            let click_oid = new mongodb.ObjectID(click.ops[0]._id)
+
+            await User.findOneAndUpdate(
+                { _id: matched_clicking_user_oid },
+                { $push: { clicks: click_oid } },
+            )
+            new_click_oids.push(click_oid)
+        }
 
         // Update clicks array for clicking user
-        await User.findByIdAndUpdate(clicking_user, 
-            { $push: { clicks: { $each: new_click_ids } } }
-        )
-
         // Archive the event for the clicking user
-        await User.updateOne({ _id: clicking_user },
-            { $pullAll: { reviewable_events: [reviewed_event] } }
+        await User.findOneAndUpdate(
+            { _id: clicking_user_oid },
+            { 
+                $addToSet: { clicks: { $each: new_click_oids } }, 
+                $pull: { reviewable_events: reviewed_event_oid },
+                $push: { archived_events: reviewed_event_oid }
+            }
         )
-
-        await User.updateOne({ _id: clicking_user },
-            { $push: { archived_events: reviewed_event } }
-        )
-
-        res.status(201).json({
-            createdOptIns: new_opt_ins,
-            generatedClicks: new_clicks,
-            usersToNotify: matched_opt_ins
+        res.status(200).json({
+            newClicks: new_click_oids
         })
-
     } catch (error) {
         console.log(error)
         res.status(500).json({
@@ -100,13 +85,25 @@ router.post('/:userId/reviewEvent/:eventId', async (req, res, next) => {
     }
 })
 
+// Retrieves active events for a given user
 router.get('/:id/activeEvents', async (req, res, next) => {
-    // console.log('here')
-    const id = req.params.id
+    const id = new mongodb.ObjectID(req.params.id)
     try {
-        let active_events = await User.findById(id)
-        .populate('active_events')
-        .select('active_events');
+        let active_events = await User.aggregate([
+            { $project: {
+                "active_events": 1
+            }},
+            { $match: {
+                "_id": id
+            }},
+            { $lookup: {
+                from: "events",
+                localField: "active_events",
+                foreignField: "_id",
+                as: "active_events"
+            }}
+        ]).toArray()
+        active_events = active_events[0].active_events
 
         res.status(200).json(active_events)
     } catch (error) {
@@ -117,12 +114,25 @@ router.get('/:id/activeEvents', async (req, res, next) => {
     }
 })
 
+// Retrieves reviewable events for a given user
 router.get('/:id/reviewableEvents', async (req, res, next) => {
-    const id = req.params.id
+    const id = new mongodb.ObjectID(req.params.id)
     try {
-        let reviewable_events = await User.findById(id)
-            .populate('reviewable_events')
-            .select('reviewable_events')
+        let reviewable_events = await User.aggregate([
+            { $project: {
+                "reviewable_events": 1
+            }},
+            { $match: {
+                "_id": id
+            }},
+            { $lookup: {
+                from: "events",
+                localField: "reviewable_events",
+                foreignField: "_id",
+                as: "reviewable_events"
+            }}
+        ]).toArray()
+        reviewable_events = reviewable_events[0].reviewable_events
 
         res.status(200).json(reviewable_events)
     } catch (error) {
@@ -133,12 +143,25 @@ router.get('/:id/reviewableEvents', async (req, res, next) => {
     }
 })
 
+// Retrieves archived events for a given user
 router.get('/:id/archivedEvents', async (req, res, next) => {
-    const id = req.params.id
+    const id = new mongodb.ObjectID(req.params.id)
     try {
-        let archived_events = await User.findById(id)
-            .populate('archived_events')
-            .select('archived_events')
+        let archived_events = await User.aggregate([
+            { $project: {
+                "archived_events": 1
+            }},
+            { $match: {
+                "_id": id
+            }},
+            { $lookup: {
+                from: "events",
+                localField: "archived_events",
+                foreignField: "_id",
+                as: "archived_events"
+            }}
+        ]).toArray()
+        archived_events = archived_events[0].archived_events
 
         res.status(200).json(archived_events)
     } catch (error) {
@@ -149,11 +172,25 @@ router.get('/:id/archivedEvents', async (req, res, next) => {
     }
 })
 
+// Retrieves click objects for a given user
 router.get('/:id/clicks', async (req, res, next) => {
-    const id = req.params.id
+    const id = new mongodb.ObjectID(req.params.id);
     try {
-        let clicks = await User.findById(id)
-            .populate('clicks')
+        let clicks = await User.aggregate([
+            { $project: {
+                "clicks" : 1
+            }},
+            { $match: {
+                "_id": id
+            }}, 
+            { $lookup: {
+                from: "clicks",
+                localField: "clicks",
+                foreignField: "_id",
+                as: "clicks"
+            }}
+        ]).toArray()
+        clicks = clicks[0].clicks
 
         res.status(200).json(clicks)
     } catch (error) {
@@ -166,9 +203,13 @@ router.get('/:id/clicks', async (req, res, next) => {
 
 // Retreives user by id
 router.get('/:id', async (req, res, next) => {
-    const id = req.params.id
+    // const id = req.params.id
+    const id = new mongodb.ObjectID(req.params.id);
     try {
-        const user = await User.findById(id)
+        const user = await User.findOne({
+            _id: id
+        })
+
         if (user) {
             res.status(200).json(user)
         } else {
@@ -186,23 +227,21 @@ router.get('/:id', async (req, res, next) => {
 
 // Creates new clickn user account
 router.post('/', async (req, res, next) => {
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        full_name: req.body.full_name,
-        profile_image_path: req.body.profile_image_path,
-        oauth: {
-            service: req.body.oauth_service,
-            service_username: req.body.oauth_service_username,
-        },
-        clicks: [],
-        active_events: [],
-        reviewable_events: [],
-        archived_events: []
-    })
     try {
-        await user.save()
+        let user = await User.insertOne({
+            full_name: req.body.full_name,
+            profile_image_path: req.body.profile_image_path,
+            outh: {
+                service: req.body.oauth_service,
+                service_username: req.body.oauth_service_username
+            },
+            clicks: [],
+            active_events: [],
+            reviewable_events: [],
+            archived_events: []
+        });
         res.status(201).json({
-            createdUser: user
+            createdUser: user.ops[0]
         })
     } catch (error) {
         console.log(error)
@@ -214,7 +253,7 @@ router.post('/', async (req, res, next) => {
 
 // Removes user by id
 router.delete('/:id', async (req, res, next) => {
-    const id = req.params.id
+    const id = new mongodb.ObjectID(req.params.id)
     try {
         let result = await User.remove({ _id: id })
         console.log(result)
@@ -228,16 +267,42 @@ router.delete('/:id', async (req, res, next) => {
 })
 
 // Updates supplied user properties
-router.patch('/:id', async (req, res, next) => {
-    const id = req.params.id
-    const updateOps = {}
-    for (const ops of req.body) {
-        updateOps[ops.propName] = ops.value;
-    }
+// router.patch('/:id', async (req, res, next) => {
+//     const id = req.params.id
+//     const updateOps = {}
+//     for (const ops of req.body) {
+//         updateOps[ops.propName] = ops.value;
+//     }
+//     try {
+//         let result = await User.update({ _id: id }, { $set: updateOps })
+//         console.log(result)
+//         res.status(200).json(result)
+//     } catch (error) {
+//         res.status(500).json({
+//             error: error
+//         })
+//     }
+// })
+
+// Adds user to existing event
+router.patch('/:userId/joinEvent/:eventId', async (req, res, next) => {
+    const user_oid = new mongodb.ObjectID(req.params.userId)
+    const event_oid = new mongodb.ObjectID(req.params.eventId)
     try {
-        let result = await User.update({ _id: id }, { $set: updateOps })
-        console.log(result)
-        res.status(200).json(result)
+        const user = await User.findOneAndUpdate(
+            { _id: user_oid },
+            { $push: { active_events: event_oid } },
+            { returnOriginal: false }
+        )
+        const event = await Event.findOneAndUpdate(
+            { _id: event_oid },
+            { $push: { users: user_oid } },
+            { returnOriginal: false }
+        )
+        res.status(200).json({
+            updatedUser: user.value,
+            updatedEvent: event.value
+        })
     } catch (error) {
         res.status(500).json({
             error: error
@@ -245,27 +310,5 @@ router.patch('/:id', async (req, res, next) => {
     }
 })
 
-router.patch('/:userId/joinEvent/:eventId', async (req, res, next) => {
-    const userId = req.params.userId
-    const eventId = req.params.eventId
-    try {
-        const user = await User.findByIdAndUpdate(userId,
-            { $push: { active_events: eventId } 
-        })
-        const event = await Event.findByIdAndUpdate(eventId,
-            { $push: { users: userId } 
-        })
-        res.status(200).json(
-            {
-                updatedUser: user,
-                updatedEvent: event
-            }
-        )
-    } catch (error) {
-        res.status(500).json({
-            error: error
-        })
-    }
-})
 
 module.exports = router
